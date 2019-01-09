f87554098798f0dea4e030eba167dd07acbc2790
Mickey Poblete <mickey@Setups-MacBook-Pro.local>
2019-01-09 11:21:41 -0800

    changed PORT to 3000

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var db = require("./models");
 var passport = require("./config/passport");
 
 var app = express();
-var PORT = process.env.PORT || 3306;
+var PORT = process.env.PORT || 3000;
 
 // Middleware
 app.use(bodyParser.urlencoded({ extended: false }));
