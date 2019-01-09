b9faf55628f8163b375e335a29ae205635499d96
jarrenpatao <jarrenpatao@gmail.com>
2019-01-08 22:04:36 -0800

    added new handlebars for page navigation, will work on account.handlebars next

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -22,6 +22,16 @@ module.exports = function(app) {
     });
   });
 
+  // Load sign up page
+  app.get("/signup", function(req, res) {
+    db.Example.findAll({}).then(function(dbExamples) {
+      res.render("signup", {
+        msg: "BillY",
+        examples: dbExamples
+      });
+    });
+  });
+
   // Render 404 page for any unmatched routes
   app.get("*", function(req, res) {
     res.render("404");
