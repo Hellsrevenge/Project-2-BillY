faba0e9c96132123fe789aaac197ee2cc861cc8b
Mickey Poblete <mickey@Setups-MacBook-Pro.local>
2019-01-11 12:40:19 -0800

    commit before working on username for authentication purposes

--- public/js/login.js
+++ public/js/login.js
@@ -28,7 +28,8 @@ $(document).ready(function() {
       email: email,
       password: password
     }).then(function(data) {
-      window.location.href = "/hello";
+      console.log(data)
+      window.location.href = "/account/:id";
       // If there's an error, log the error
     }).catch(function(err) {
       console.log(err);
