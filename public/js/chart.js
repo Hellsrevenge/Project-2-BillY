a3b5f1e91b04ece8abd9eade8f50401bae2f1b19
Hellsrevenge <ivark26@gmail.com>
2019-01-13 22:25:20 -0800

    update chart and add bill input

--- public/js/chart.js
+++ public/js/chart.js
@@ -6,13 +6,11 @@ var chart = new Chart(ctx, {
   // The data for our dataset
   data: {
     labels: [
-      "Utility",
+      "Traffic Tickets",
       "Fines",
-      "Entertainment",
-      "Grocery",
-      "Restaurants",
-      "Auto",
-      "Clothing"
+      "Others",
+      "Medical bills",
+
     ],
     datasets: [
       {
@@ -21,13 +19,10 @@ var chart = new Chart(ctx, {
           "rgb(255, 99, 132)",
           "rgb(196, 219, 185)",
           "rgb(77, 201, 253)",
-          "rgb(247, 193, 64)",
-          "rgb(138, 238, 102)",
-          "rgb(250, 251, 10)",
           "rgb(138, 151, 194)"
         ],
         borderColor: "rgb(255, 255, 255)",
-        data: [130, 77.68, 212.12, 81.66, 340.29, 30.75, 65.01]
+        data: [130, 77.68, 212.12, 81.66]
       }
     ]
   },
