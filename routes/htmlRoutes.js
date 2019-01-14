940c514db8893a80de87751b582fe1e65fe7bb14
Hellsrevenge <ivark26@gmail.com>
2019-01-13 20:09:55 -0800

    Add autcreation of sample bills

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -49,22 +49,34 @@ module.exports = function(app) {
   });
 
   app.post("/signup", function(req, res) {
+    if (!req.body.user_name || !req.body.email || !req.body.password) {
+      req.flash("error", "Missing credentials");
+      res.redirect("/signup");
+    }
+
     db.Users.findOne({
       where: {
         email: req.body.email
       }
     }).then(function(dbUser) {
       if (dbUser) {
-        req.flash("error", "User " +  dbUser.email +" already exists");
+        req.flash("error", "User " + dbUser.email + " already exists");
         res.redirect("/signup");
       } else {
         db.Users.create({
           user_name: req.body.user_name,
           email: req.body.email,
           password: req.body.password
-        }).then(function(data) {
-          req.flash("info", "Thank you for registration. You can login with your email and password.");
-          res.redirect("/login");
+        }).then(function(user) {
+          db.Bills.bulkCreate(
+            db.Bills.getSeedBills(user)
+          ).then(function(response) {
+            req.flash("info", "Thank you for registration. You can login with your email and password.");
+            res.redirect("/login");
+          }).catch(function(error) {
+            req.flash("error", error[0]);
+            res.redirect("/signup");
+          });
         }).catch(function(err) {
           req.flash("error", err);
           res.redirect("/signup");
